668edb12e3debfdfab4697d7621284e441332f92
Manuel José Moral Eliche <az.az.az.mjme@gmail.com>
2021-03-18 23:20:24 +0100

    picasso: Build hidl

--- device.mk
+++ device.mk
@@ -100,6 +100,9 @@ PRODUCT_PACKAGES += \
     bt_configstore.conf
 
 # Camera
+PRODUCT_PACKAGES += \
+    android.hidl.memory.block@1.0
+
 PRODUCT_COPY_FILES += \
     hardware/interfaces/camera/provider/2.4/default/android.hardware.camera.provider@2.4-service_64.rc:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init/android.hardware.camera.provider@2.4-service_64.rc
 
